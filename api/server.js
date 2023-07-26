2efc2c7062421cc8b50f8941184597e5e5d7e1ee
Terrell Booker <terrellbooker@gmail.com>
2023-07-26 02:30:55 -0400

    initial commit

--- api/server.js
+++ api/server.js
@@ -14,6 +14,10 @@ server.use(cors());
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 
+server.get('/', (req, res) => {
+  res.json({message: "api up"})
+})
+
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
